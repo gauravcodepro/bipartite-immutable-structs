f58237a1cf02cb8fd9c40c09fe4cd59c0eb0c4ee
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-07-02 00:04:10 +0200

    Update geneStructview.jl 2024-7-1
    added the first part of the code and going to finish this tomorrow. A mutable struct based approach to visualize reads faster on the spanning genome annotation as well as implementation in pangenomes.
    
    - part the sam in the pangenome read mapping, hash the start and then put a forward pointer and then store the other reads as the second iter. if the forward pointer match to the splice!(readstring, i) position, return the pointer.

--- geneStructview.jl
+++ geneStructview.jl
@@ -13,10 +13,20 @@ mutable struct gene
   samLoc::Int8
   samStart::Union{Int8, Float16}
 end 
+mutable struct sam
+  samstart::Union{Int8, Float64}
+  samend::Union{Int8, Float64}
+  junctionreadcount::Union{Int8, Float64}
+  uniquereadcount::Union{Int8, Float64}
+  readstring::String
+  readseq::String
+end 
 
 readSAM = readlines(samfile)
 storeloc = gene[]
 for i in readlines(readSAM)
   push(storeloc, gene(readsam[i].split[1], readsam[i].split[2], readsam[i].split[4])
     # add a splat notation. 
+
+  
   
