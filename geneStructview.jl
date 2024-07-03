969714689828e250917dcdf5c00489cf025280cf
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-07-03 16:40:46 +0200

    2024-7-3 new closure function coded and added
    added a new closure function and adding the histo analysis to the Kmer for the genome scape analysis.

--- geneStructview.jl
+++ geneStructview.jl
@@ -76,5 +76,53 @@ function samReadsAll(samfile, variantfile)
             end
         end
     end
+
+
+function samReadsKmerPlot(samfile, variantfile)
+    # reading the sam for the variants and indexing the variants as Any[] and then mapping the reads to variants by the position
+    # iteration over the range of the start and the stop and if the read start iter is present
+    # in the range of the mapped position extract the read and annotate the read
+    # no memory loading needed making the annotations faster.
+    # using Int64 so that no buffer overflow occurs.
+    readmapStart = Any[]
+    readmapEnd = Any[]
+    readMIDTags = Any[]
+    readString = String[]
+    readfile = readlines(open(samfile))
+    for i in 1:length(readfile)
+        if !startswith(readfile[i], "@")
+            push!(readmapStart,split(split(readfile[i], r"\t")[1], r"_")[3])
+            push!(readmapEnd, split(split(readfile[i], r"\t")[1], r"_")[4])
+            push!(readMIDTags,split(readfile[i], r"\t")[6])
+            push!(readString, split(readfile[i], r"\t")[10])
+        end
+    end
+    varaintPos = Any[]
+    readvariants = readlines(open(variantfile))
+    for i in 1:readvariants
+        if !startswith(readvariants[i], r'#|##')
+          push!(variantPos, split(readvariants[i], "\t")[2])
+        end
+    end
+   iterstart = [(parse(Int64,readmapStart[i]),parse(Int64,readmapEnd[i]), readString[i]) for i in 1:length(readmapStart)]
+   getReads = Any[]
+    for i in 1:length(iterstart)
+        for j in 1:length(variantPos)
+            if iterstart[i][1] < variantPos[j] && iterstart[i][2] > variantPos[j]
+                push!(getReads, iterStart[i])
+            end
+        end
+    end
+    function kmerplot(frequency)
+        ### adding a closure here for the kmer so that it can be called for any stored variable with in the function.
+        selreadString = [getReads[i][3] i in 1:length(getReads)]
+        selreadsStart = [getReads[i][1] i in 1:length(getReads)]
+        selreadEnd = [getReads[i][2] i in 1:length(getReads)]
+        storingsKmer = Any[]
+        @asset length(selreadsStart) == length(selreadEnd) == length(selreadString)
+        for i in 1:length(selreadstring)-parse(Int64,frequency)
+            push!(storingsKmer, [selreadStart[i], selreadEnd[i], selreadString[i:i+parse(Int64, frequency)]
+        end
+    end
 end
 
